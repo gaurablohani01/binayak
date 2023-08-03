4cc750c85699a9a03c1ae37fce280fc0ec780a86
Gaurab Lohani <sales@gaurablohani.info.np>
2023-08-03 18:48:01 +0545

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// var typingEffect = new Typed(".multi-line",{
-//          strings: ["Web Developer", "Youtuber", "Designer", "Student"],
-//           loop: true,
-//            backSpeed:80,
-//             typeSpeed:100,
-//             backDelay: 1500
-//     })
-
-
-
-$('.navbar-nav>li>a').on('click', function () {
-    $('.navbar-collapse').collapse('hide');
-});
+$('.navbar-nav>li>a').on('click', function () { $('.navbar-collapse').collapse('hide'); });
